ecec2fa638682b1ef367ffb4e6de03e588216788
hosung77 <ho4728@gmail.com>
2024-08-16 14:52:46 +0900

    svelte, vite로 바꾼 후 firebase deploy 세팅

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Login from "./pages/Login.svelte";
+  import Main from "./pages/Main.svelte";
+  import Notfound from "./pages/Notfound.svelte";
+  import Signup from "./pages/Signup.svelte";
+  import Write from "./pages/Write.svelte";
+  import Router from 'svelte-spa-router'
+  import "./css/style.css";
+
+  const routes = {
+    '/':Main,
+    '/login':Login,
+    '/signup':Signup,
+    '/Write':Write,
+    "*":Notfound
+  }
+</script>
+
+<Router routes={routes}/>
+
+
+<main>  <!--html부분-->
+<div></div>
+</main>
+
+<style> 
+  div{
+    color:red;
+  }
+</style>
